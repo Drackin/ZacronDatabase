7083e7bb400b4f4502e0025d413ebcad0198807f
DrackinBest <sonaycannet10@gmail.com>
2021-12-20 20:10:17 +0300

    Fast, Useful and Easy JSON Database

--- obj/Debug/net6.0/ZacronDatabase.AssemblyInfo.cs
+++ obj/Debug/net6.0/ZacronDatabase.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZacronDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZacronDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZacronDatabase")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
 
